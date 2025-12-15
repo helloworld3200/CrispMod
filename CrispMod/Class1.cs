264ff68c391c831d88dc2cf854c51b7bdb323a1d
helloworld3200 <helloworld32.tech@gmail.com>
2025-12-15 13:02:07 +0000

    Add project files.

---
+++ CrispMod/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+
+namespace Pistachio
+{
+    public class CrispMod : IUserMod
+    {
+        public string Name 
+        {
+            get { return "Crisp"; }
+        }
+
+        public string Description 
+        {
+            get { return "A graphical fidelity overhaul, by pistachiocrisps."; }
+        }
+    }
+}
